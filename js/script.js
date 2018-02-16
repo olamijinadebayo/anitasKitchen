c6094d5e7b7d40e000b808fa4162d6b93ec74cc3
olamijinadebayo <consultolamijin@gmail.com>
2018-02-16 15:17:34 +0300

    Layout for the first section
    this will insert backgound image and text in the first section of the page

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-$(document).ready(function() {
-  $("h3").click(function(e) {
-    $(".text").toggleClass("show");
-e.preventDefault();
+$(document).ready(function () {
 
-  });
 });
-
-
-
-
-
-// $(document).ready(function() {
-//   $("h3").on("click", function() {
-//     $(this).parent().find('h3.active').removeClass('active');
-//     $(this).addClass('active');
-//
-//     $(".text").toggleClass("show");
-//   });
-// });
